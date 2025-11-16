10ff00888d16d7c5b94e5464124707b7f029cf91
LangstonW430 <langstonw430@gmail.com>
2025-11-15 21:16:34 -0500

    calculate ramaining time method in task class

--- task.py
+++ task.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 class Task:
-    def __init__(self, title="Untitled Task", due_date=None, status="not started"):
+    def __init__(self, title, due_date=None, status="not started"):
         self.title = title
         self.due_date = due_date
         self.status = status
+        self.remaining_time = self.calculate_remaining_time()
 
     def to_dict(self):
         return {
@@ -10,6 +13,11 @@ class Task:
             "due_date": self.due_date,
             "status": self.status
         }
+    
+    def calculate_remaining_time(self):
+        if self.due_date:
+            return self.due_date - datetime.now()
+        return None
     
     @staticmethod
     def from_dict(data):
